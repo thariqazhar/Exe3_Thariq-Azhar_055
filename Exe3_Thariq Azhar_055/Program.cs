5509b90335ec6fe79e09144fa3b0cf7c399da168
TC1055 <thariq.azhar.ft21@mail.umy.ac.id>
2022-11-23 16:48:37 +0700

    Create a method for adding node

--- Exe3_Thariq Azhar_055/Program.cs
+++ Exe3_Thariq Azhar_055/Program.cs
@@ -25,5 +25,57 @@ namespace Exercise_Linked_List_A
         {
             LAST = null;
         }
+
+        /*Create a method for adding node*/
+        public void addnode()
+        {
+            int nim;
+            string nm;
+            Console.WriteLine("\nEnter the roll number of the student: ");
+            nim = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("\nEnter the name of the student");
+            nm = Console.ReadLine();
+            Node newNode = new Node();
+            newNode.rollNumber = nim;
+            newNode.name = nm;
+
+            if (LAST == null)
+            {
+                newNode.next = LAST;
+                LAST = newNode;
+                return;
+            }
+            else if (nim <= LAST.rollNumber)
+            {
+                if (LAST != null && nim == LAST.rollNumber)
+                {
+                    Console.WriteLine("\nDuplicate student number are not allowed");
+                    return;
+                }
+                newNode.next = LAST;
+                LAST = newNode;
+                return;
+            }
+            Node previous, current;
+            for (current = previous = LAST; current != null && nim >= current.rollNumber; previous = current, current = current.next)
+            {
+                if (nim == current.rollNumber)
+                {
+                    Console.WriteLine("\nDuplicate roll numbers not allowed");
+                    return;
+                }
+            }
+            newNode.next = current;
+            newNode.prev = previous;
+
+            if (current == null)
+            {
+                newNode.next = null;
+                previous.next = newNode;
+                return;
+            }
+            current.prev = newNode;
+            previous.next = newNode;
+        }
     }
 }
