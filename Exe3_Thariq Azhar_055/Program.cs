75f98cc028e6f2af640944dcea4ee78f0fc653d6
TC1055 <thariq.azhar.ft21@mail.umy.ac.id>
2022-11-23 16:45:17 +0700

    declare a pointer LAST which always point to the last node

--- Exe3_Thariq Azhar_055/Program.cs
+++ Exe3_Thariq Azhar_055/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Versioning;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,9 +10,20 @@ namespace Exercise_Linked_List_A
 {
     class Node
     {
-        /*creates Nodes for the circular mexted list*/
+        /*creates Nodes for the circular nexted list*/
         public int rollNumber;
         public string name;
         public Node next;
+        public Node prev;
+    }
+
+    class CircularList
+    {
+        Node LAST;
+
+        public CircularList()
+        {
+            LAST = null;
+        }
     }
 }
